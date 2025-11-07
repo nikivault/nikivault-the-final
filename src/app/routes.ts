236dd097799bc372e9f1c1630698f74dbf4ea947
nikivault <nikithasudeer@gmail.com>
2025-11-07 20:35:17 +0530

    revert: restore original routes.ts after attempted fix

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,7 +5,7 @@ import {
 	type RouteConfigEntry,
 	index,
 	route,
-} from '@react-router/fs-routes';
+} from '@react-router/dev/routes';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
@@ -117,4 +117,3 @@ const notFound = route('*?', './__create/not-found.tsx');
 const routes = [...generateRoutes(tree), notFound];
 
 export default routes;
-
